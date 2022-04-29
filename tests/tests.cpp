57bc0c8254ea8b6979afc08d93838ab5f5414dc4
indecisivemagician <perry.192@wright.edu>
2022-04-29 16:36:08 -0400

    added test for issue 9

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,6 +1,13 @@
 #include <gtest/gtest.h>
 
+//Example Test
 TEST(Assert, empty) { GTEST_ASSERT_EQ(true, true); }
+
+//[issue] https://github.com/rhartlage10/CtrlAltElite-Craft/issues/9
+TEST(Gravity, TestJumpKey) {
+	GTEST_ASSERT_EQ(CRAFT_KEY_JUMP, GLFW_KEY_SPACE);
+}
+
 
 int main(int argc, char* argv[])
 {
