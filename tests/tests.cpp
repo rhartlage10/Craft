38d566865f1c0636a7735ceec9258c005cc6ac35
indecisivemagician <perry.192@wright.edu>
2022-04-29 16:51:23 -0400

    added needed includes for used variables

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,9 +1,16 @@
 #include <gtest/gtest.h>
 
+//includes from main.c
+#include <GLFW/glfw3.h>
+#include <gl/glew.h>
+#include <string.h>
+#include <curl/curl.h>
+#include "config.h" //for key presses
+
 //Example Test
 TEST(Assert, empty) { GTEST_ASSERT_EQ(true, true); }
 
-//[issue] https://github.com/rhartlage10/CtrlAltElite-Craft/issues/9
+//[issue] (https://github.com/rhartlage10/CtrlAltElite-Craft/issues/9)
 TEST(Gravity, TestJumpKey) {
 	GTEST_ASSERT_EQ(CRAFT_KEY_JUMP, GLFW_KEY_SPACE);
 }
